c0c97446a35d2cee789ee477ef99e137cfcbbb3e
Tsaplin.A.V <tsaplin.a.v@gmail.com>
2016-01-27 16:29:01 +0600

    recursion algorithm done

--- Java/MavenLabirint/src/test/java/PassabilityOfLabiritntsTests.java
+++ Java/MavenLabirint/src/test/java/PassabilityOfLabiritntsTests.java
@@ -32,11 +32,11 @@ public class PassabilityOfLabiritntsTests {
 		
 		// MyClass is tested
 		StrategyRecursion startTestPassRecursion = new StrategyRecursion(mapPass);
-//		StrategyRecursion startTestNonPassRecursion = new StrategyRecursion(mapNonPass);
+		StrategyRecursion startTestNonPassRecursion = new StrategyRecursion(mapNonPass);
 		// assert statements
 		Point start = new Point(2,0);
 		assertEquals("equal true", true, startTestPassRecursion.findNewWay(start));
-//		assertEquals("equal false", false, startTestNonPassRecursion.findNewWay(2, 0));
+		assertEquals("equal false", false, startTestNonPassRecursion.findNewWay(start));
 	}
 }
 
