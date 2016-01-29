393626e09fd17ba854b7204f19b980f0b24660b8
Mikhail Tsaplin <mikhail@cellwize.com>
2016-01-29 17:25:23 +0600

    Renaming Test classes

--- Java/MavenLabirint/src/test/java/PassabilityOfLabiritntsTest.java
+++ Java/MavenLabirint/src/test/java/PassabilityOfLabiritntsTest.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class PassabilityOfLabiritntsTests {
+public class PassabilityOfLabiritntsTest {
 
 	@Test
 //	public void test() {
--- Java/MavenLabirint/src/test/java/PointTest.java
+++ Java/MavenLabirint/src/test/java/PointTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 
 
-public class PointTests {
+public class PointTest {
 
 	Direction dirTestLeft = Direction.left;
 	
