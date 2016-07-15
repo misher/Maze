ac42aa19d1a430e678db1f3b4031ee4a952f7afa
Tsaplin.A.V <tsaplin.a.v@gmail.com>
2016-07-15 19:52:05 +0600

    remote unneeded folders

--- Java/ChatByTCPUpd/src/main/java/org/chat/common/chatSessionIdTable.java
+++
@@ -1,7 +0,0 @@
-package org.chat.common;
-
-/**
- * Created by A.V.Tsaplin on 14.07.2016.
- */
-public class chatSessionIdTable {
-}
--- Java/ChatByTCPUpd/src/main/java/org/chat/persistence/HibernateUtil.java
+++
@@ -1,7 +0,0 @@
-package org.chat.persistence;
-
-/**
- * Created by A.V.Tsaplin on 14.07.2016.
- */
-public class HibernateUtil {
-}
