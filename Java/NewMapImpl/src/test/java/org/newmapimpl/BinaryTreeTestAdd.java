d27e626e40d8dc2686a34d48ee59cb809e5b7151
Tsaplin.A.V <tsaplin.a.v@gmail.com>
2016-08-19 19:03:55 +0600

    with test

---
+++ Java/NewMapImpl/src/test/java/org/newmapimpl/BinaryTreeTestAdd.java
@@ -0,0 +1,27 @@
+package org.newmapimpl;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ *
+ * Created by A.V.Tsaplin on 19.08.2016.
+ */
+
+public class BinaryTreeTestAdd {
+
+    @Test
+    public void binaryTreeTestAdd() {
+        BinaryApex startBinaryApex = new BinaryApex(0, 0);
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.add(startBinaryApex, 12, 14);
+        binaryTree.add(startBinaryApex, -12, 15);
+        assertEquals("right point test ", 14, startBinaryApex.lookAtRight().getValue());
+        assertEquals("leftt point test ", 15, startBinaryApex.lookAtLeft().getValue());
+        binaryTree.add(startBinaryApex,6, 7);
+        assertEquals("right point test in generation ", 7, startBinaryApex.lookAtRight().lookAtLeft().getValue());
+    }
+
+
+}
---
+++ Java/NewMapImpl/src/test/java/org/newmapimpl/BinaryTreeTestGet.java
@@ -0,0 +1,27 @@
+package org.newmapimpl;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ *
+ * Created by A.V.Tsaplin on 19.08.2016.
+ */
+
+public class BinaryTreeTestGet {
+
+    @Test
+    public void binaryTreeTestGet() {
+        BinaryApex startBinaryApex = new BinaryApex(0, 0);
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.add(startBinaryApex, 12, 14);
+        binaryTree.add(startBinaryApex, -12, 15);
+        binaryTree.add(startBinaryApex,6, 7);
+
+        assertEquals("get method test", 7, binaryTree.get(startBinaryApex, 6));
+
+    }
+
+}
+
